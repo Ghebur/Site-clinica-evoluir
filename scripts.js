80397d3d2783bd93156f82d33ba55a8d88b3f2bd
Ghebur <glauciusfilho90@gmail.com>
2026-09-04 17:02:58 -0300

    Location created, profissionals photos added, and some minor adjustments to the services section.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,3 @@
-var swiper = new Swiper(".slider-content", {
-        slidesPerView: 2,
-        spaceBetween: 25,
-        loop: true,
-        fade: true,
-        grabCursor: true,
-        pagination: {
-          el: '.prof-pagination',
-          clickable: true,
-          dynamicBullets: true,
-        },  
-        navigation: {
-          nextEl: '.prof-next',
-          prevEl: '.prof-prev',
-        },
-     
-
-      breakpoints:{
-        0: {
-          slidesPerView: 1,
-        },
-        520: {
-          slidesPerView: 2,
-        },
-        950: {
-          slidesPerView: 2,
-        },
-      }
-     });
 
 var swiper = new Swiper('.ambientes-slider', {
         pagination: {
